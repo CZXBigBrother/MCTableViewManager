f2bda84a9a771f22530683a3e15ceef84c5f6199
marcoChen <chenxingghost@gmail.com>
2017-10-30 09:43:36 +0800

    添加注释

--- MCTableViewManager/MCTableViewManager.h
+++ MCTableViewManager/MCTableViewManager.h
@@ -12,12 +12,43 @@
 @interface MCTableViewManager : NSObject
 @property (strong, nonatomic) NSMutableArray *GlobalData;
 
+/**
+ 返回cell
+
+ @param indexPath 和系统返回的indexPath相同
+ @param target 执行方法的对象
+ @return 对象
+ */
 - (instancetype)MC_cellForRowAtIndexPath:(NSIndexPath *)indexPath withTarget:(id)target;
 
+/**
+ 返回cell高度
+
+ @param indexPath 和系统返回的indexPath相同
+ @return 高度
+ */
 - (CGFloat)MC_cellheightForRowAtIndexPath:(NSIndexPath *)indexPath;
+
+/**
+ 返回cell点击事件
+
+ @param indexPath 和系统返回的indexPath相同
+ @param target 执行方法的对象
+ */
 - (void)MC_didSelectRowAtIndexPath:(NSIndexPath *)indexPath withTarget:(id)target;
 
+/**
+ 返回队列组数
+
+ @return 返回队列组数
+ */
 - (NSInteger)MC_numberOfSectionsInTableView;
+
+/**
+ 返回队列组中的个数
+
+ @param section 组index
+ @return 返回队列组中的个数
+ */
 - (NSInteger)MC_numberOfRowsInSection:(NSInteger)section;
-//- (id)productFunc:(id)params withTarget:(id)sender withSelector:(SEL)selector;
 @end
